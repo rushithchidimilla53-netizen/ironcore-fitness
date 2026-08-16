fa975752a6786d926e9263a1f4fdc7438267c885
rushith <rushithchidimilla53@gmail.com>
2026-08-16 11:42:22 +0530

    update the settings

--- fitness_website/settings.py
+++ fitness_website/settings.py
@@ -73,35 +73,41 @@ STATICFILES_DIRS = [
     BASE_DIR / 'fitness' / 'static',
 ]
 
-# EMAIL_BACKEND = 'fitness.resend_email_backend.ResendEmailBackend'
-
-# RESEND_API_KEY = os.getenv('RESEND_API_KEY', '')
-
-# RESEND_FROM_EMAIL = os.getenv(
-#     'RESEND_FROM_EMAIL',
-#     'onboarding@resend.dev'
-# )
-
-# DEFAULT_FROM_EMAIL = RESEND_FROM_EMAIL
-# SERVER_EMAIL = RESEND_FROM_EMAIL
-# ADMIN_EMAIL = os.getenv('ADMIN_EMAIL', '')
-
 EMAIL_BACKEND = os.getenv(
-    'EMAIL_BACKEND',
-    'django.core.mail.backends.smtp.EmailBackend'
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.smtp.EmailBackend"
 )
 
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST = os.getenv(
+    "EMAIL_HOST",
+    "smtp.gmail.com"
+)
 
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = int(
+    os.getenv("EMAIL_PORT", "587")
+)
+
+EMAIL_USE_TLS = os.getenv(
+    "EMAIL_USE_TLS",
+    "True"
+).lower() == "true"
+
+EMAIL_HOST_USER = os.getenv(
+    "EMAIL_HOST_USER",
+    ""
+)
+
+EMAIL_HOST_PASSWORD = os.getenv(
+    "EMAIL_HOST_PASSWORD",
+    ""
+)
 
 DEFAULT_FROM_EMAIL = os.getenv(
-    'DEFAULT_FROM_EMAIL',
+    "DEFAULT_FROM_EMAIL",
     EMAIL_HOST_USER
 )
+
+EMAIL_TIMEOUT = 20
 
 
 
